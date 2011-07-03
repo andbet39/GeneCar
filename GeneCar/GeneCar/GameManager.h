b609fdb5b48bbf3c5b10228d3370ee64a578ba75
andbet39 <andrea.terzani@gmail.com>
2011-07-03 10:20:45 +0200

    First creation of gamemanager

---
+++ GeneCar/GeneCar/GameManager.h
@@ -0,0 +1,13 @@
+//
+//  GameManager.h
+//  GeneCar
+//
+//  Created by Andrea Terzani on 03/07/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GameManager : NSObject
+
+@end
