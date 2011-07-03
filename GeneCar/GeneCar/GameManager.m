5b719104332f1a9645e5dae5aed5267fc1edee07
andbet39 <andrea.terzani@gmail.com>
2011-07-03 10:20:59 +0200

    first

---
+++ GeneCar/GeneCar/GameManager.m
@@ -0,0 +1,23 @@
+//
+//  GameManager.m
+//  GeneCar
+//
+//  Created by Andrea Terzani on 03/07/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "GameManager.h"
+
+@implementation GameManager
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
