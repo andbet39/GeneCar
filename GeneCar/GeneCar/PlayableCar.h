d7dba6584d032e3d62fa6958afc4fabf0a9caf77
andbet39 <andrea.terzani@gmail.com>
2011-07-04 08:37:26 +0200

    first load

---
+++ GeneCar/GeneCar/PlayableCar.h
@@ -0,0 +1,18 @@
+//
+//  PlayableCar.h
+//  GeneCar
+//
+//  Created by Andrea Terzani on 03/07/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "car.h"
+
+@interface PlayableCar : car
+
+
+
+-(void) update;
+
+@end
---
+++ GeneCar/GeneCar/PlayableCar.mm
@@ -0,0 +1,32 @@
+//
+//  PlayableCar.m
+//  GeneCar
+//
+//  Created by Andrea Terzani on 03/07/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "PlayableCar.h"
+
+@implementation PlayableCar
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+-(void) update{
+
+
+
+
+
+}
+
+
+@end
